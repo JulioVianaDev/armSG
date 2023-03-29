b7bd28e2be8a7b1bd4758ef8a6d0ec00d3cc362e
JulioVianaDev <JulioVianaDev@gmail.com>
2023-03-29 13:17:49 -0300

    base ok

--- armSG.ino
+++ armSG.ino
@@ -1,28 +1,62 @@
 #include <Servo.h>
 Servo servoBase; 
+Servo servoEsquerda;
+Servo servoDireita;
+Servo servoGarra; 
 #include <ResponsiveAnalogRead.h>
 const int ANALOG_PIN_BASE = A0;
-ResponsiveAnalogRead analogbase(ANALOG_PIN_BASE, true);
+const int ANALOG_PIN_ESQUERDA = A1;
+const int ANALOG_PIN_DIREITA = A2;
+const int ANALOG_PIN_GARRA = A3;
+ResponsiveAnalogRead analogBase(ANALOG_PIN_BASE, true);
+ResponsiveAnalogRead analogEsquerda(ANALOG_PIN_ESQUERDA, true);
+ResponsiveAnalogRead analogDireita(ANALOG_PIN_DIREITA, true);
+ResponsiveAnalogRead analogGarra(ANALOG_PIN_GARRA, true);
 // define the pin you want to use
 void setup()
 {
   // Anexa o Servo ao Pin5
   servoBase.attach(5); 
+  //servoEsquerda.attach(6); 
+  //servoDireita.attach(9); 
+  //servoGarra.attach(10); 
   Serial.begin(9600);
 }
  
 void loop()
 {
-  analogbase.update();
+  analogBase.update();
+  //analogEsquerda.update();
+  //analogDireita.update();
+  //analogGarra.update();
   // Lê o valor do Potenciometro
   //int angleBase = analogRead(0); 
   //int angleBaseConvertido=map(angleBase, 0, 1023, 0, 180);
-  int anguloFiltrado = analogbase.getValue();
-  int angleBaseFiltradoConvertido=map(anguloFiltrado, 0, 1023, 0, 180);
   
-//  Serial.print(angleBase);
- // Serial.print("\t");
-  //Serial.println(anguloFiltrado);
+  //Angulos Servo Base
+  int anguloFiltradoBase = analogBase.getValue();
+  int angleBaseFiltradoConvertido=map(anguloFiltradoBase, 0, 1023, 0, 180);
+  
+  // Angulos Servo Esquerda
+  //int anguloFiltradoEsquerda = analogEsquerda.getValue();
+  //int angleEsquerdaFiltradoConvertido=map(anguloFiltradoEsquerda, 0, 1023, 0, 180);
+  
+  // Angulos Servo Direita
+  //int anguloFiltradoDireita = analogDireita.getValue();
+  //int angleDireitaFiltradoConvertido=map(anguloFiltradoDireita, 0, 1023, 0, 180);
+  
+  // Angulos Servo Garra
+  //int anguloFiltradoGarra = analogGarra.getValue();
+  //int angleGarraFiltradoConvertido=map(anguloFiltradoGarra, 0, 1023, 0, 180);
+  
+  // PLOTTER SERIAL PARA VER OS RUIDOS
+  //Serial.print(angleBase);
+  //Serial.print("\t");
+  //Serial.println(anguloFiltradoBase);
+  
   servoBase.write(angleBaseFiltradoConvertido); 
+  //servoEsquerda.write(angleEsquerdaFiltradoConvertido);
+  //servoDireita.write(angleDireitaFiltradoConvertido);
+  //servoGarra.write(angleGarraFiltradoConvertido);
   delay(60);
 }
